3a78da5c1e3d6fd67354ec3f0a624f4484caa272
colacadstink <benharris5@gmail.com>
2023-12-24 23:21:03 -0500

    Refactor component into folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {CardDetailsComponent} from "./card-details/card-details.component";
+import {CardDetailsComponent} from "./components/card-details/card-details.component";
 
 export const routes: Routes = [{
   path: 'card/:setID/:cardNum',
