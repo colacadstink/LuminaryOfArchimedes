85a4b92d31ab1b11f1a8a4c548055504ca9b4099
colacadstink <benharris5@gmail.com>
2023-12-29 16:47:46 -0500

    Remove unused import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import {provideRouter, withComponentInputBinding, withHashLocation} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideServiceWorker } from '@angular/service-worker';
 
