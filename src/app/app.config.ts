afb8279cccb6d871186ca4a94c4cc74552e7572f
colacadstink <benharris5@gmail.com>
2023-12-29 14:24:13 -0500

    Service worker & custom icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {provideRouter, withComponentInputBinding, withHashLocation} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding(), withHashLocation()), provideClientHydration(), provideAnimations()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
+    provideClientHydration(),
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
 };
